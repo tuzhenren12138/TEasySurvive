9521a5218bc746dc9526528f678f27a9a164fb01
zhouqishi <zhouqishi@eastcompeace.com>
2024-11-19 14:08:15 +0800

    init

---
+++ src/client/java/com/tuzhenren12138/TEasySurviveClient.java
@@ -0,0 +1,10 @@
+package com.tuzhenren12138;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TEasySurviveClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
